74cb89bae0d081af19b2e1b488e7aa73ef0e8543
Laedoan <92416258+Laedoan@users.noreply.github.com>
2021-10-15 23:02:54 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip3 install pandas;
+pip3 install numpy;
+pip3 install matplotlib;
+pip3 install random;
+pip3 install sklearn;
+pip3 install pillow;
+
+python3 ./classify_clothing/Classify_images_of_clothing.py;
